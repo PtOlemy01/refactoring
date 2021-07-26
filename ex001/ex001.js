9787da30f4229131fd924a37a766d6b514144c79
PtOlemy01 <shapewki@naver.com>
2021-07-26 16:20:28 +0900

    amountFor() change

--- ex001/ex001.js
+++ ex001/ex001.js
@@ -34,6 +34,34 @@ function statement(invoice, plays){
     function enrichPerformance(aPerformance){
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
         result.play = playFor(result);
+        result.amount = amountFor(result);
+        return result;
+    }
+
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (aPerformance.play.type) {
+            case "tragedy": //비극
+            result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy": // 희극 
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
+        }
         return result;
     }
 
@@ -42,7 +70,7 @@ function statement(invoice, plays){
     
         for(let perf of data.performances){
             // 청구 내역을 출력한다.
-            result += `  ${perf.play.name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += `  ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
         }
     
         result += `총액 : ${usd(totalAmount())}\n`;
@@ -53,7 +81,7 @@ function statement(invoice, plays){
         function totalAmount(){
             let result = 0;
             for (let perf of data.performances){
-                result += amountFor(perf);
+                result += perf.amount;
             }
             return result;
         }
@@ -85,33 +113,7 @@ function statement(invoice, plays){
             return result;
         }
     
-        function playFor(aPerformance){
-            return plays[aPerformance.playID];
-        }
-    
-        function amountFor(aPerformance) {
-            let result = 0;
-    
-            switch (aPerformance.play.type) {
-                case "tragedy": //비극
-                result = 40000;
-                    if (aPerformance.audience > 30) {
-                        result += 1000 * (aPerformance.audience - 30);
-                    }
-                    break;
-                case "comedy": // 희극 
-                    result = 30000;
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 * (aPerformance.audience - 20);
-                    }
-                    result += 300 * aPerformance.audience;
-                    break;
-                default:
-                    throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
-            }
-            return result;
-        } // renderPlainText() end
-    } // amountFor() end
+    } // renderPlainText() end
 } // statement() end
 
 console.log(statement(invoices, plays));
