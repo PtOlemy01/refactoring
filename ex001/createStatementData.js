5d49e0e0f63ab049bedd3c39494aaffbe911e998
PtOlemy01 <shapewki@naver.com>
2021-07-26 17:50:37 +0900

    create calculator

--- ex001/createStatementData.js
+++ ex001/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator{
+    constructor(aPerformance, aPlay){
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+}
+
 export default function createStatementData(invoice, plays) {
     const statementData = {};
 
@@ -17,6 +24,7 @@ export default function createStatementData(invoice, plays) {
     }
     
     function enrichPerformance(aPerformance){
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
         result.play = playFor(result);
         result.amount = amountFor(result);
