a1065dacaadf6bceaed6b24752e4a8c1cd6b7a43
PtOlemy01 <shapewki@naver.com>
2021-07-26 17:20:04 +0900

    split file

---
+++ ex001/statement.js
@@ -0,0 +1,56 @@
+import createStatementData from './createStatementData.js';
+
+let plays = {
+    "hamlet" : {"name" : "Hamlet", "type" :"tragedy"},
+    "as-like" : {"name" : "As You Like It", "type" : "comedy"},
+    "othello" : {"name" : "Othello", "type" : "tragedy"}
+};
+
+let invoices = [
+    {
+        "customer" : "BigCo",
+        "performances" : [
+            {
+                "playID" : "hamlet",
+                "audience" : 55
+            },
+            {
+                "playID" : "as-like",
+                "audience" : 35
+            },
+            {
+                "playID" : "othello",
+                "audience" : 40
+            }
+        ]
+    }
+];
+
+function statement(invoice, plays){
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data, plays){
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for(let perf of data.performances){
+        // 청구 내역을 출력한다.
+        result += `  ${perf.play.name} : ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액 : ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
+
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {
+                style: "currency", currency: "USD",
+                minimumFractionDigits: 2
+            }).format(aNumber/ 100);
+    }
+}
+
+console.log(statement(invoices, plays));
+
