dc15934d741df6a0fa06676c955e896ec61ef4dc
PtOlemy01 <shapewki@naver.com>
2021-07-26 16:16:57 +0900

    playFor change

--- ex001/ex001.js
+++ ex001/ex001.js
@@ -33,6 +33,7 @@ function statement(invoice, plays){
 
     function enrichPerformance(aPerformance){
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+        result.play = playFor(result);
         return result;
     }
 
@@ -41,7 +42,7 @@ function statement(invoice, plays){
     
         for(let perf of data.performances){
             // 청구 내역을 출력한다.
-            result += `  ${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+            result += `  ${perf.play.name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         }
     
         result += `총액 : ${usd(totalAmount())}\n`;
@@ -78,7 +79,7 @@ function statement(invoice, plays){
             let result = 0;
     
             result += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" === playFor(aPerformance).type)
+            if ("comedy" === aPerformance.play.type)
                 result += Math.floor(aPerformance.audience / 5);
     
             return result;
@@ -91,7 +92,7 @@ function statement(invoice, plays){
         function amountFor(aPerformance) {
             let result = 0;
     
-            switch (playFor(aPerformance).type) {
+            switch (aPerformance.play.type) {
                 case "tragedy": //비극
                 result = 40000;
                     if (aPerformance.audience > 30) {
@@ -106,7 +107,7 @@ function statement(invoice, plays){
                     result += 300 * aPerformance.audience;
                     break;
                 default:
-                    throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+                    throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
             }
             return result;
         } // renderPlainText() end
