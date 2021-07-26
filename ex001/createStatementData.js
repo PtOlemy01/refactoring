3934d1d82eb1a77a50be6a7e61ed18fbe543c07b
PtOlemy01 <shapewki@naver.com>
2021-07-26 17:59:15 +0900

    replace volumeCredits

--- ex001/createStatementData.js
+++ ex001/createStatementData.js
@@ -26,6 +26,16 @@ class PerformanceCalculator{
         }
         return result;
     }
+
+    get volumeCredits(){
+        let result = 0;
+    
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type)
+            result += Math.floor(this.performance.audience / 5);
+    
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -49,28 +59,14 @@ export default function createStatementData(invoice, plays) {
     function enrichPerformance(aPerformance){
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
     
     function playFor(aPerformance){
         return plays[aPerformance.playID];
-    }
-    
-    function amountFor(aPerformance) {
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-    
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-    
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-    
-        return result;
     }
 }
 
