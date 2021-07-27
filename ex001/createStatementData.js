91ca35734377a0d06d112a14f12c4b7fc2fa185e
PtOlemy01 <shapewki@naver.com>
2021-07-27 13:46:52 +0900

    change position

--- ex001/createStatementData.js
+++ ex001/createStatementData.js
@@ -1,3 +1,44 @@
+export default function createStatementData(invoice, plays) {
+    const statementData = {};
+
+    statementData.customer = invoice[0].customer;
+    statementData.performances = invoice[0].performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
+    return statementData;
+
+    function enrichPerformance(aPerformance){
+        const calculator = createPerfomanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+    
+    function playFor(aPerformance){
+        return plays[aPerformance.playID];
+    }
+    
+    function totalAmount(data){
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+    
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+    }
+}
+
+function createPerfomanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type){
+        case "tragedy" : return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+        default :
+            throw new Error(`알 수 없는 장르 : ${aplay.type}`);
+    }
+}
+
 class PerformanceCalculator{
     constructor(aPerformance, aPlay){
         this.performance = aPerformance;
@@ -35,47 +76,6 @@ class ComedyCalculator extends PerformanceCalculator{
 
     get volumeCredits(){
         return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
-}
-
-function createPerfomanceCalculator(aPerformance, aPlay){
-    switch(aPlay.type){
-        case "tragedy" : return new TragedyCalculator(aPerformance, aPlay);
-        case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
-        default :
-            throw new Error(`알 수 없는 장르 : ${aplay.type}`);
-    }
-}
-
-export default function createStatementData(invoice, plays) {
-    const statementData = {};
-
-    statementData.customer = invoice[0].customer;
-    statementData.performances = invoice[0].performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-
-    return statementData;
-
-    function totalAmount(data){
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
-    
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
-    }
-    
-    function enrichPerformance(aPerformance){
-        const calculator = createPerfomanceCalculator(aPerformance, playFor(aPerformance));
-        const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-        result.play = calculator.play;
-        result.amount = calculator.amount;
-        result.volumeCredits = calculator.volumeCredits;
-        return result;
-    }
-    
-    function playFor(aPerformance){
-        return plays[aPerformance.playID];
     }
 }
 
