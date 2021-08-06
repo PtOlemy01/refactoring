b9bfaf54de1af208bb0331784a9e3f82fa3cc576
PtOlemy01 <shapewki@naver.com>
2021-08-06 10:55:22 +0900

    inline function change

--- ex6/6_2.js
+++ ex6/6_2.js
@@ -1,7 +1,4 @@
 function getRating(driver){
-    return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+    return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(driver){
-    return driver.numberOfLateDeliveries > 5;
-}
