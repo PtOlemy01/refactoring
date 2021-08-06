94db8e706b1b78731d8bae80b4d82ad2f8d08f0a
PtOlemy01 <shapewki@naver.com>
2021-08-06 10:59:01 +0900

    inline function test2

--- ex6/6_2.js
+++ ex6/6_2.js
@@ -1,4 +1,16 @@
-function getRating(driver){
-    return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
+// function getRating(driver){
+//     return (driver.numberOfLateDeliveries > 5) ? 2 : 1;
+// }
+
+function reportLines(aCustomer){
+    const lines = [];
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+    gatherCustomerData(lines, aCustomer);
+    return lines;
 }
 
+function gatherCustomerData(out, aCustomer){
+    
+    
+}
