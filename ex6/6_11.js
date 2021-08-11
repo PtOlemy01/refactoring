09c81c0e04ca069fd22ce2980ddf0d640cd8fa6a
PtOlemy01 <shapewki@naver.com>
2021-08-11 17:53:57 +0900

    Split Phase ex2

--- ex6/6_11.js
+++ ex6/6_11.js
@@ -22,15 +22,15 @@
 function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discoutRate;
-    
-    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    const priceData = {basePrice : basePrice, quantity : quantity};
+    const price = applyShipping(priceData, shippingMethod,  discount);
     
     return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount){
-    const shippingPerCase = (basePrice > shippingMethod.discount) ? shippingMethod.discountFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount * shippingCost;
+function applyShipping(priceData, shippingMethod,  discount){
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discount) ? shippingMethod.discountFee : shippingMethod.feePerCase;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    const price = priceData.basePrice - discount * shippingCost;
     return price;
 }
