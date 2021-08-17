29479f6b5da8423061f31c316a1d96f475c43a2d
PtOlemy01 <shapewki@naver.com>
2021-08-17 17:00:34 +0900

    72

---
+++ ex7/7_2.js
@@ -0,0 +1,35 @@
+// class Person{
+//     get course() {return this._course;}
+//     set course(aList) {this._course = aList;}
+// }
+
+// class Person{
+//     get course() {return this._course.slice();}
+//     addCourse(aCourse){...}
+//     removeCourse(aCourse){...}
+// }
+
+class Person{
+    constructor(name){
+        this._name = name;
+        this._course = [];
+    }
+    get name() {return this._name;}
+    get course() {return this._course}
+    set course(aList){this._course = aList;}
+}
+
+class Course{
+    constructor(name, isAdvanced){
+        this._name = name;
+        this._isAdvanced = isAdvanced;
+    }
+    get name() {return this._name;}
+    get isAdvanced(){return this._isAdvanced;}
+}
+
+Person aPerson = new Person();
+
+numAdvancedCourses = aPerson.course
+    .filter(c => c.isAdvanced)
+    .length;
