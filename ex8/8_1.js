70c2fffab7b595eda0bca418e5d3d0ade0e5cde1
PtOlemy01 <shapewki@naver.com>
2021-08-20 16:47:42 +0900

    Move function

--- ex8/8_1.js
+++ ex8/8_1.js
@@ -35,3 +35,23 @@
 // function radians(degrees){
 //     return degrees * Math.PI / 180;
 // } 
+
+class Account{
+    get bankCharge(){ // 은행 이자 계산
+        let result = 4.5;
+        if(this._daysOverdrawn > 0) result += this.overdrafeCharge;
+        return result;
+    }
+
+    get overdrafeCharge(){ // 초과 인출 이자 계산
+        if(this.type.isPremium){
+            const baseCharge = 10;
+            if(this.daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+        }
+        else
+            return this.daysOverdrawn * 1.75;
+    }
+}
