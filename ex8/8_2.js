9f856e82c302977e4aa7c29cbad8f1e7afdab811
PtOlemy01 <shapewki@naver.com>
2021-08-20 17:15:23 +0900

    Move filed

--- ex8/8_2.js
+++ ex8/8_2.js
@@ -1,23 +1,27 @@
 class Customer{
     constructor(name, discountRate){
         this._name = name; 
-        this._setDiscountRate(discountRate);
         this._contract = new CustomerContract(dateToday() );
+        this._setDiscountRate(discountRate);
     }
 
-    get discountRate() {return this._discountRate;}
-    _setDiscountRate(aNumber) {this._discountRate = aNumber;}
+    get discountRate() {return this._contract._discountRate;}
+    _setDiscountRate(aNumber) {this._contarct._discountRate = aNumber;}
     becomePreferred(){
         this._setDiscountRate( this.discountRate += 0.03);
         // 다른 멋진 일들
     }
     applyDicount(amount){
-        return amount.subtract(amount.multiply(this._discountRate));
+        return amount.subtract(amount.multiply(this.discountRate));
     }
 }
 
 class CustomerContract{
-    constructor(startDate){
+    constructor(startDate, discountRate){
         this._startDate = startDate;
+        this._discountRate = discountRate;
     }
+
+    get discountRate() {return this._discountRate;}
+    set discountRate(arg) {this._discountRate = arg;}
 }
