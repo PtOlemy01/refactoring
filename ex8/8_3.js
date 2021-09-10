406a7a82126ef9c09567371f94c68f381d550fb0
PtOlemy01 <shapewki@naver.com>
2021-09-10 17:31:38 +0900

    ex 8-3 first source

--- ex8/8_3.js
+++ ex8/8_3.js
@@ -6,3 +6,19 @@ function renderPerson(outStream, person){
     result.push(emitPhotoData(person.photo));
     return result.join("\n");
 }
+
+function photoDiv(p){
+    return [
+        "<div>",
+        `<p>제목: ${p.title}</p>`,
+        emitPhotoData(p),
+        "</div>",
+    ].join("\n");
+}
+
+function emitPhotoData(aPhoto){
+    const result = [];
+    result.push(`<p>위치:${aPhoto.location}</p>`);
+    result.push(`<p>날짜:${aPhoto.date.toDateString()}</p>`);
+    return result.join("\n");
+}
