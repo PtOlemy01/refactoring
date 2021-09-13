484d8e2241736ad65b21f85f3a7105290db7c2c0
PtOlemy01 <shapewki@naver.com>
2021-09-13 13:18:24 +0900

    slide statement base code

---
+++ ex8/8_6.js
@@ -0,0 +1,8 @@
+const pricingPlan = retrievPricingPlan();
+const order = retreiveOrder();
+const baseCharge = pricingPlan.base;
+let charge;
+const chargePerUnit = pricingPlan.unit;
+const units = order.units;
+let discount;
+charge = baseCharge + units * chargePerUnit;
