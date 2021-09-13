3a56d9be25d03e4f8c3bf357a5449e4343020197
PtOlemy01 <shapewki@naver.com>
2021-09-13 17:06:01 +0900

    inline and remove dead code

--- ex9/9_3.js
+++ ex9/9_3.js
@@ -1,16 +1,10 @@
 class ProductionPlan {
     get production() {
-        assert(this._production === this.calculatedProduction);
-        return this._production;
-    }
-
-    get calculatedProduction(){
         return this._adjustMents
             .reduce((sum, a) => sum + a.amount, 0);
     }
 
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
-        this._production += anAdjustment.amount;
     }
 }
