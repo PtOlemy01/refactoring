da4e0b1cc6adcd494ebc84c75403ce298b1e5586
PtOlemy01 <shapewki@naver.com>
2021-09-13 17:51:18 +0900

    불변으로 만들기.

--- ex9/9_4.js
+++ ex9/9_4.js
@@ -4,12 +4,21 @@ class Person{
     }
 
     get officeAreaCode()        {return this._telephoneNumber.areaCode};
-    set officeAreaCode(arg)     {this._telephoneNumber.areaCode = arg;}
+    set officeAreaCode(arg)     {
+        this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
+    }
     get officeNumber()          {return this._telephoneNumber.number;}
-    set officeNumber(arg)       {this._telephoneNumber.number = arg;}
+    set officeNumber(arg)       {
+        this._telephoneNumber.number = new TelephoneNumber(this.officeAreaCode, arg);
+    }
 }
 
 class TelephoneNumber {
+    constructor(areaCode, number) {
+        this._areaCode = areaCode;
+        this._number = number;
+    }
+
     get areaCode()      {return this._areaCode;}
     set areaCode(arg)   {this._areaCode = arg;}
     get number()        {return this._number;}
