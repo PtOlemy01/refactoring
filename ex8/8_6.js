08c07e20540b023315e0773f6689480aa1e18410
PtOlemy01 <shapewki@naver.com>
2021-09-13 13:27:24 +0900

    slide statement slice

--- ex8/8_6.js
+++ ex8/8_6.js
@@ -1,12 +1,12 @@
 const pricingPlan = retrievPricingPlan();
-const order = retreiveOrder();
 const baseCharge = pricingPlan.base;
 let charge;
 const chargePerUnit = pricingPlan.unit;
+const order = retreiveOrder();
 const units = order.units;
-let discount;
 charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+let discount;
 discount = discountableUnits * pricingPlan.dicountFactor;
 if(order.isRepeat) discount += 20;
 charge = charge - discount;
