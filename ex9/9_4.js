0cda390053938028d50fdb6f5169103e2979c3f7
PtOlemy01 <shapewki@naver.com>
2021-09-13 17:52:10 +0900

    불변으로 만들기.

--- ex9/9_4.js
+++ ex9/9_4.js
@@ -18,9 +18,4 @@ class TelephoneNumber {
         this._areaCode = areaCode;
         this._number = number;
     }
-
-    get areaCode()      {return this._areaCode;}
-    set areaCode(arg)   {this._areaCode = arg;}
-    get number()        {return this._number;}
-    set number(arg)     {this._number = arg;}
 }
