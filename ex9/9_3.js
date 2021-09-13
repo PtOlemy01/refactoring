9281adfede319b371fa63fffa16b395b7dd5c5f0
PtOlemy01 <shapewki@naver.com>
2021-09-13 17:12:19 +0900

    add assertion

--- ex9/9_3.js
+++ ex9/9_3.js
@@ -6,8 +6,14 @@ class ProductionPlan {
     }
 
     get production() {
+        assert(this._productionAccumulator === this.calculateProductionAccmulator);
         return this._initialProduction + this._productionAccumulator;
     }
+    get calculateProductionAccmulator(){
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0);
+    }
+
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
