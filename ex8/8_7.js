717aef72a71cbea5723bf076b7adba463657d0f8
PtOlemy01 <shapewki@naver.com>
2021-09-13 13:35:41 +0900

    function extract

--- ex8/8_7.js
+++ ex8/8_7.js
@@ -12,3 +12,11 @@ function youngAgeAndTotalSalary(){
 
     return `최연소:${youngest}, 총 급여 : ${totalSalary}`;
 }
+
+function totalSalary(){
+    return people.reduce((total, p) => total + p.salary, 0);
+}
+
+function youngestAge(){
+    return Math.min(...people.map(p => p.age));
+}
