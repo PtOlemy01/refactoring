6c7d43ccb696a113f78bfefd0b66e1c117ea2192
PtOlemy01 <shapewki@naver.com>
2021-09-14 11:40:31 +0900

    함수로 추출하기.

--- ex10/10_2.js
+++ ex10/10_2.js
@@ -1,7 +1,11 @@
 function disabilityAmount(anEmployee){
-    if((anEmployee.seniority < 2)
-        || (anEmployee.monthsDisabled > 12)
-        || (anEmployee.isPartTime)) return 0;
+    function isNotEligibleForEisability() {
+        return (anEmployee.seniority < 2)
+            || (anEmployee.monthsDisabled > 12)
+            || (anEmployee.isPartTime);
+    }
+
+    if(isNotEligibleForEisability()) return 0;
 
     // 장애 수당 계산
 }
