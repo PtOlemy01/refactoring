46849f8650b53385058493c6946c587e545f0fae
PtOlemy01 <shapewki@naver.com>
2021-09-14 11:47:47 +0900

    보호구문으로 변경.2

--- ex10/10_3.js
+++ ex10/10_3.js
@@ -1,18 +1,14 @@
 function payAmount(employee){
     let result;
     if(employee.isSeparated) return { amount : 0, reasonCode : "SEP"};
-    else{
-        if(employee.isRetired){ // 은퇴한 직원인가 ?
-            result = {amount : 0, reasonCode : "RET"};
-        }
-        else{
-            // 급여 계산 로직
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            sed.do.eiusmod = tempor.incididnut.ut(labore) && dolore(manga.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
-    }
+    if(employee.isRetired) return {amount : 0, reasonCode : "RET"};
+
+    // 급여 계산 로직
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididnut.ut(labore) && dolore(manga.aliqua);
+    ut.enim.ad(minim.veniam);
+    result = someFinalComputation();
+
     return result;
 }
