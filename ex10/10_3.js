ad57b2a70f7e69d67508028f7c17b8327b06faf4
PtOlemy01 <shapewki@naver.com>
2021-09-14 15:03:52 +0900

    보호 구문으로 변경.

--- ex10/10_3.js
+++ ex10/10_3.js
@@ -14,10 +14,10 @@ function payAmount(employee){
 // 예시 : 조건 반대로 만들기 ?
 function adjustedCapital(anInstrument){
     let result = 0;
-    if(anInstrument.capital > 0){
-        if(anInstrument.interestRate > 0 && anInstrument.duration > 0){
-            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        }
+    if(anInstrument.capital <= 0) return result;
+    if(anInstrument.interestRate > 0 && anInstrument.duration > 0){
+        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
+
     return result;
 }
