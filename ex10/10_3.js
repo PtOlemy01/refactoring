2d1a915d753d9d67a11116d471156643a0b5c149
PtOlemy01 <shapewki@naver.com>
2021-09-14 11:46:12 +0900

    중첩 조건문을 보호 구문으로 바꾸기 예제 코드

---
+++ ex10/10_3.js
@@ -0,0 +1,20 @@
+function payAmount(employee){
+    let result;
+    if(employee.isSeparated){       // 퇴사한 직원인가 ?
+        result = { amount : 0, reasonCode : "SEP"};
+    }
+    else{
+        if(employee.isRetired){ // 은퇴한 직원인가 ?
+            result = {amount : 0, reasonCode : "RET"};
+        }
+        else{
+            // 급여 계산 로직
+            lorem.ipsum(dolor.sitAmet);
+            consectetur(adipiscing).elit();
+            sed.do.eiusmod = tempor.incididnut.ut(labore) && dolore(manga.aliqua);
+            ut.enim.ad(minim.veniam);
+            result = someFinalComputation();
+        }
+    }
+    return result;
+}
