8a1dc516fab3c0a5cd6ed2bb8528c5a04d3717e6
PtOlemy01 <shapewki@naver.com>
2021-09-14 15:08:36 +0900

    result 변수 제거.

--- ex10/10_3.js
+++ ex10/10_3.js
@@ -13,8 +13,8 @@ function payAmount(employee){
 
 // 예시 : 조건 반대로 만들기 ?
 function adjustedCapital(anInstrument){
-    let result = 0;
-    if(anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
-    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-    return result;
+    if(anInstrument.capital <= 0
+        || anInstrument.interestRate <= 0
+        || anInstrument.duration <= 0) return result;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
