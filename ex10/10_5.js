899867786dde6934d0cf39e2cb214e80a677def5
PtOlemy01 <shapewki@naver.com>
2021-09-15 14:26:45 +0900

    특이케이스 추가하기.

---
+++ ex10/10_5.js
@@ -0,0 +1,12 @@
+class Site{
+    get customer() {return this._customer;}
+}
+
+class Customer{
+    get name () {}
+    get billingPlan() {}
+    set billingPlan(arg) {}
+    get paymentHistory() {}
+}
+
+// 음... 모르겠다...
