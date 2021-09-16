75241273ab0d36e09dc338b418960737f0a7fce2
PtOlemy01 <shapewki@naver.com>
2021-09-16 15:39:34 +0900

    파이프 라인으로 변경.

--- ex10/10_7.js
+++ ex10/10_7.js
@@ -3,15 +3,5 @@ checkForMiscreants(people);
 // 생략
 
 function checkForMiscreants(people){
-    for(const p of people){
-        if(p === "조커"){
-            sendAlert();
-            return;
-        }
-        if(p === "사루만") {
-            sendAlert();
-            return;
-        }
-
-    }
+    if(people.some(p=> ["조커", "사루만"].includes(p))) sendAlert();
 }
