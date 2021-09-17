896e85ec58b22480ba17f48373f2a6c575889664
PtOlemy01 <shapewki@naver.com>
2021-09-17 15:15:56 +0900

    메서드까지 인라인.

--- ex11/11_6.js
+++ ex11/11_6.js
@@ -1,17 +1,16 @@
 class HeatingPlan{
-    get targetTemperature(){
-        return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
-    }
 
-    xxNEWtargetTemperature(selectedTemperature) {
+    targetTemperature(selectedTemperature) {
         if (selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
     }
 }
 
-if(thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) >
+const thePlan = new HeatingPlan();
+
+if(thePlan.targetTemperature(thermostat.selectedTemperature) >
     thermostat.currentTemperature) seltToHeat();
-else if (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) <
+else if (thePlan.targetTemperature(thermostat.selectedTemperature) <
     thermostat.currentTemperature) seltToCool();
 else setOff();
