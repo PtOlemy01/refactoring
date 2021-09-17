856d94c7397ee746f34972af83e829d0eb9f6da1
PtOlemy01 <shapewki@naver.com>
2021-09-17 14:41:46 +0900

    인라인으로 바꿔보기.

--- ex11/11_4.js
+++ ex11/11_4.js
@@ -1,11 +1,14 @@
 let aPlan = new HeatingPlan();
-const tempRange = aRoom.daysTempRange;
-const low = tempRange.low;
-const high = tempRange.high;
-const isWithRange = aPlan.withinRange(low, high);
-if(!isWithRange)
-    alerts.push("방 온도가 지정 범위를 벗어 났습니다.")
 
+if(!xxNEWwithinRange(aPlan, aRoom.daysTempRange));
+    alerts.push("방 온도가 지정 범위를 벗어 났습니다.");
+
+function xxNEWwithinRange(aPlan, tempRange){
+    const low = tempRange.low;
+    const high = tempRange.high;
+
+    return aPlan.withinRange(low, high);
+}
 class HeatingPlan{
     withinRange(bottom, top){
         return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
