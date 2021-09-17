04d7468a5db23f56730391e61ca299861d73af6c
PtOlemy01 <shapewki@naver.com>
2021-09-17 14:03:22 +0900

    모두 새함수로 교체.

--- ex11/11_4.js
+++ ex11/11_4.js
@@ -1,8 +1,7 @@
 let aPlan = new HeatingPlan();
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
-if(!aPlan.withinRange(low, high))
-    alerts.push("방 온도가 지정 범위를 벗어 났습니다.")
+
+if(!aPlan.xxNEWWithinRange(aRoom.daysTempRange))
+    alerts.push("방 온도가 지정 범위를 벗어 났습니다.");
 
 class HeatingPlan{
     withinRange(bottom, top){
