a2a0aa4bad2e51ec395fdcbc9b29de9139e1516f
PtOlemy01 <shapewki@naver.com>
2021-09-17 15:10:46 +0900

    메서드로 추출하기.

--- ex11/11_6.js
+++ ex11/11_6.js
@@ -1,9 +1,12 @@
 class HeatingPlan{
     get targetTemperature(){
         const selectedTemperature = thermostat.selectedTemperature;
+        return this.xxNEWtargetTemperature(selectedTemperature);
+    }
 
-        if(selectedTemperature > this._max) return this._max;
-        else if(selectedTemperature < this._min)return this._min;
+    xxNEWtargetTemperature(selectedTemperature) {
+        if (selectedTemperature > this._max) return this._max;
+        else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
     }
 }
