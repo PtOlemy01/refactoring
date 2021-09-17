0b47e500e880fa47f4906cc2108eb675b4db6f2c
PtOlemy01 <shapewki@naver.com>
2021-09-17 14:04:56 +0900

    함수 인라인후 함수 정리.

--- ex11/11_4.js
+++ ex11/11_4.js
@@ -1,14 +1,11 @@
 let aPlan = new HeatingPlan();
 
-if(!aPlan.xxNEWWithinRange(aRoom.daysTempRange))
+if(!aPlan.withinRange(aRoom.daysTempRange))
     alerts.push("방 온도가 지정 범위를 벗어 났습니다.");
 
 class HeatingPlan{
-    withinRange(bottom, top){
-        return (bottom >= this._temperatureRange.low) && (top <= this._temperatureRange.high);
-    }
-
-    xxNEWWithinRange(aNumberRange){
-        return this.withinRange(aNumberRange.low, aNumberRange.high);
+    withinRange(aNumberRange){
+        return (aNumberRange.low >= this._temperatureRange.low)
+            && (aNumberRange.high <= this._temperatureRange.high);
     }
 }
